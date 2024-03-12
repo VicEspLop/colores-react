84162dc4c7035d1779a86a96e2886a981514ec4e
Victor <>
2024-03-12 14:00:13 +0100

    colores-react

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Colores from './Colores.jsx'
+import './estilo.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Colores />
+)
